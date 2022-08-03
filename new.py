15e7614cd88b1023d08bd14c47d20a7b12ae4c32
ganyoobi <commeymark56@gmail.com>
2022-08-03 15:35:46 +0000

    'new'

--- new.py
+++ new.py
@@ -4,4 +4,5 @@ def largest_sum(payments):
             largest = payments[i] + payments[i + 1] + payments[i + 3] + payments[i + 4] + payments[i + 5]
                 consecutive5 = f"{payments[i]} + {payments[i + 1]} + {payments[i + 2]} + {payments[i + 3]} + {payments[i + 4]}"
                     n = len(payments)
-                    
+                        if n > 5:
+                            
