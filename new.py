83df5ecb3c76fd7b78be85fedc5d6d8d69c5f8b6
ganyoobi <commeymark56@gmail.com>
2022-08-03 15:35:32 +0000

    'new'

--- new.py
+++ new.py
@@ -1,4 +1,5 @@
 #printing the sum of five consecutive payments in a list
 def largest_sum(payments):
         i = 0
-        
+            largest = payments[i] + payments[i + 1] + payments[i + 3] + payments[i + 4] + payments[i + 5]
+            
