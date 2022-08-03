e61a6d7ff50fafd607a5d0da2cd22dd8e254ab38
ganyoobi <commeymark56@gmail.com>
2022-08-03 15:27:35 +0000

    'new'

--- new.py
+++ new.py
@@ -28,4 +28,6 @@ mport subprocess
 def largest_sum(payments):
         i = 0
             largest = payments[i] + payments[i + 1] + payments[i + 3] + payments[i + 4] + payments[i + 5]
-            
+                consecutive5 = f"{payments[i]} + {payments[i + 1]} + {payments[i + 2]} + {payments[i + 3]} + {payments[i + 4]}"
+                    n = len(payments)
+                    
